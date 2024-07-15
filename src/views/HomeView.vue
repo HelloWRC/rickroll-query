8c0324a0273dd4d5ffff759f9998b1049a5654a7
WRC <hello_wrc@outlook.com>
2024-07-15 12:20:31 +0800

    feat: 免责声明

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -39,6 +39,10 @@ function copyLink() {
     </VAppBarTitle>
   </VAppBar>
   <VSheet class="form-sheet">
+    <VAlert type="warning" icon="mdi-alert" title="警告" class="mb-4">
+      本项目仅供娱乐，使用时请遵守相关法律法规，用户使用本项目造成的任何后果均由用户承担。继续使用本项目即代表您同意本项目的 <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">开源许可（AGPL-v3 License）</a>。
+      我们不会存储您使用本项目过程中产生的任何数据。如果您对上述文本存在异议，请立刻关闭本页面。
+    </VAlert>
     <div class="gc-4 d-flex flex-column">
       <VForm>
         <VTextField label="查询名称"
@@ -53,7 +57,7 @@ function copyLink() {
         </VBtn>
 
       </VForm>
-      <div v-show="isQueryCreated">
+      <div v-show="isQueryCreated" class="mt-4 mb-4">
         <div class="ga-4 d-flex flex-column gc-4">
           <VAlert type="success">创建成功！快复制下面的链接去整蛊你的朋友吧！</VAlert>
           <VTextField v-model="queryUrl"
